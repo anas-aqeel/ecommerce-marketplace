59076a135389de2f6b8320f65d1ba3cd34304e14
unknown <anas.452111@gmail.com>
2023-06-11 11:03:31 +0500

    Router updated

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,10 +3,9 @@ import { SignUp } from '@clerk/nextjs'
 export default function Component() {
   return (
     <>
-    <div className='flex min-h-screen justify-center items-center'>
-
-      <SignUp redirectUrl={'/sign-up'}/>
-    </div>
+      <div className="flex min-h-screen justify-center items-center">
+        <SignUp redirectUrl={'/sign-up'} />
+      </div>
     </>
   )
 }
